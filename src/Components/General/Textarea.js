2778d23ff141ee54914b3435a9847d674340829a
dmls <danmlsv@gmail.com>
2020-04-22 15:28:40 -0400

    Add markdown parser to General_Textarea

--- src/Components/General/Textarea.js
+++ src/Components/General/Textarea.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import marked from 'marked';
+import {Container, Row, Col} from 'react-bootstrap';
 
 class General_Textarea extends React.Component {
     render() {
-        console.log('test');
+        let data = this.props.data;
+        console.dir(data);
         return (
-            <div>{this.props.data.Content}</div>
+            <Container fluid={data.Size === 'full_width'}>
+                <Row>
+                    <Col>
+                        <div dangerouslySetInnerHTML={{__html: marked(data.Content)}}></div>
+                    </Col>
+                </Row>
+            </Container>
         )
     }
 }
