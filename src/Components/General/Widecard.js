5183c79c5a7f082275d45266bb683266ff64e71a
dmls <danmlsv@gmail.com>
2020-05-05 16:23:45 -0400

    Build wide card widget

--- src/Components/General/Widecard.js
+++ src/Components/General/Widecard.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import { Container, Row, Col, Image } from 'react-bootstrap';
 
 class General_Widecard extends React.Component {
     render() {
         let data = this.props.data;
+        console.dir(data);
         return (
-            <></>
+            <Container className="my-5">
+                <Row>
+                    {data.Image_Position !== 'right' && 
+                        <Col className="col-12 col-sm-4">
+                            <Image fluid src={window.cms_url + data.Image.url} />
+                        </Col>
+                    }
+
+                    <Col className="col-12 col-sm-8">
+                        <div className="center-vertical">
+                            <Row>
+                                <Col>
+                                    <h1>{data.Title}</h1>
+                                </Col>
+                            </Row>
+
+                            <Row>
+                                <Col>{data.Text}</Col>
+                            </Row>
+                        </div>
+                    </Col>
+
+                    {data.Image_Position === 'right' && 
+                        <Col className="col-12 col-sm-4">
+                            <Image fluid src={window.cms_url + data.Image.url} />
+                        </Col>
+                    }
+                </Row>
+            </Container>
         )
     }
 }
