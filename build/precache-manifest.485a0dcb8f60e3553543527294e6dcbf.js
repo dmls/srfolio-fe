44f36a0f603019a7ebbdb209c3fc979758d6c9c4
dmls <danmlsv@gmail.com>
2020-05-07 13:10:29 -0400

    Add build folder

---
+++ build/precache-manifest.485a0dcb8f60e3553543527294e6dcbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b39b2dcbeb0a04fae3723fd04cf19482",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d7c60ad91a94efb0e0e0",
+    "url": "./static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "af0cc1e430cb9af0c373",
+    "url": "./static/css/main.98d85f8c.chunk.css"
+  },
+  {
+    "revision": "d7c60ad91a94efb0e0e0",
+    "url": "./static/js/2.bd9c21e1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.bd9c21e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af0cc1e430cb9af0c373",
+    "url": "./static/js/main.43d1d465.chunk.js"
+  },
+  {
+    "revision": "9aa6bd7570c87c541576",
+    "url": "./static/js/runtime-main.ad493ebd.js"
+  }
+]);
